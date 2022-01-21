0514f32fc544e195e0bfc90378377dd42ff6c65d
Ryan Ross <ryanross@Bretts-MBP.attlocal.net>
2022-01-21 11:14:01 -0600

    finished extra credit

--- neighborhood.js
+++ neighborhood.js
@@ -1,7 +1,13 @@
 let recButton = document.querySelector("#randomRec");
 
 recButton.addEventListener("click", () => {
-  const restArray = ["Thai Thai", `Harvey B's`, `Jimmy's Food Store`];
+  const restArray = [
+    "Thai Thai",
+    `Harvey B's`,
+    `Jimmy's Food Store`,
+    "Aca Las Tortas",
+    "Cook Your Own Food",
+  ];
   let rand = restArray[(Math.random() * restArray.length) | 0];
   console.log(rand);
   alert(rand);
